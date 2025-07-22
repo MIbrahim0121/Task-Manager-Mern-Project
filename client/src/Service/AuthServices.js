bbea33b42a424d3ebe7bf9858e56da13151c1609
MIbrahim0121 <hfzmibrahim295@example.com>
2025-07-22 15:42:39 -0700

    final

--- client/src/Service/AuthServices.js
+++ client/src/Service/AuthServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = "http://task-manager-mern-project-production.up.railway.app/api/v1/users"; // ✅ Backend ka sahi URL
+const API_BASE = "https://task-manager-mern-project-production.up.railway.app/api/v1/users"; // ✅ Backend ka sahi URL
 // const API_BASE2 = "http://localhost:3000/api/v1/todo";
 
 const RegisterUser = (data) => {
